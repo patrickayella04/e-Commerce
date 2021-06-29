1927bc6bc2fb3a11b6dbb2f50d40f5724a2ea7ed
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-06-29 22:18:40 +0100

    Initial implementation of first react router, with Browser router as alias

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,20 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// BrouserRouter used an alias of Router and Router is used in the app below.
+// BrowserRouter A <Router> that uses the HTML5 history API (pushState, replaceState and the popstate event) to keep your UI in sync with the URL. In order to use the router we wrapp the entire app below within it.
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import Footer from "./components/Footer"; // These components are exported by default so no need to wrap them in curly braces ie. import {Footer} from "./components/Footer";.
+import Footer from "./components/Footer"; // These Footer and Header components are exported by default so no need to wrap them in curly braces ie. import {Footer} from "./components/Footer";.
 import HomeScreen from "./screens/HomeScreen";
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         {/** Provides padding at the y axis for seperation of container and header */}
         <Container>
-          <HomeScreen />
+          <Route path="/" component={HomeScreen} exact />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
