f0b4b353b776385f9d7326fe62f187c5f53e46a4
Patrick Nyeko <patricknyeko@Patricks-MacBook-Air.local>
2021-06-30 11:57:59 +0100

    we sent a message to client with the first get request to ('/') endpoint, letting them know API is running

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 
-// We initialise express with a variable called app.
+// 1. We initialise express with a variable called app.
 const app = express();
 
-// Take that app and listen on port. Now run in the console to start the server: node backend/server. However to avoid having write this command, we create a script for start, in the root package.json. This way we can run our server with simple: npm start.
+// 3. Create a route - app.get means if we get a get request to slash ('/'), then we want to run a fuction that takes in a request and response object (req/res). The we take that res. object and send it to the client, res.send(). This will indicate what will be rended in the browser.
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+// 2. Take that app and listen on port. Now run in the console to start the server: node backend/server. However to avoid having write this command, we create a script for start, in the root package.json. This way we can run our server with simple: npm start.
 app.listen(5000, console.log("Server running on port 5000"));
